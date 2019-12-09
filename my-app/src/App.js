b1b20b890ec0d0442593ff93137267fde64ca3a9
Izzy <ibouzid3@gmail.com>
2019-12-08 22:59:33 -0500

    working website with three/two colom components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+    const [issues, setIssues] = useState("");
+
+    //chance.guid()
+
+    useEffect(()=>{
+        //TODO
+        //fetchIssues
+        //setIssues(fetched issues)
+    },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <body className="container">
+          <h1>BUG TRACKER <small>by Izzeddine Bouzid</small></h1>
+      <div className="jumbotron">
+          <h3>Add New Issue: </h3>
+          <form className="issueInputForm">
+              <div className="form-group">
+                  <label for="issueDescInput">Description</label>
+                  <input type="text"
+                         className="form-control"
+                         id="issueDescInput"
+                         placeholder="Describe the issue..."/>
+              </div>
+              <div className="form-group">
+                  <label for="issueSeverityInput">Severity</label>
+                  <select id="issueSeverityInput" className="form-control">
+                      <option value="low">Low</option>
+                      <option value="meduim">Medium</option>
+                      <option value="high">High</option>
+                  </select>
+              </div>
+              <div className="form-group">
+                  <label for="issueAssignedInput">Assigned To</label>
+                  <input type="text"
+                         className="form-control"
+                         id="issueAssignedInput"
+                         placeholder="Enter Responsible..."/>
+              </div>
+              <button type="submit" className="btn btn-primary">Add</button>
+          </form>
+          <div className="col-lg-12">
+              <div id="issuesList ">
+              </div>
+
+          </div>
+      </div>
+      <div className="footer-nav">
+          <p>Izzeddine Bouzid</p>
+      </div>
+      </body>
     </div>
   );
 }
